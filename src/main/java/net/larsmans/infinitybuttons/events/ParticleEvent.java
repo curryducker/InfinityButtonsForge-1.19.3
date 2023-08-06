41ef585534c43cd3446baeb6b116449b3c4b98da
curryducker <jur.hoekman@gmail.com>
2023-08-06 22:22:01 +0200

    sounds, server, config, torch, particle crash

--- src/main/java/net/larsmans/infinitybuttons/events/ParticleEvent.java
+++ src/main/java/net/larsmans/infinitybuttons/events/ParticleEvent.java
@@ -3,6 +3,8 @@ package net.larsmans.infinitybuttons.events;
 import net.larsmans.infinitybuttons.InfinityButtons;
 import net.larsmans.infinitybuttons.particle.InfinityButtonsParticleTypes;
 import net.larsmans.infinitybuttons.particle.custom.DiamondSparkleParticle;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.event.RegisterParticleProvidersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -10,6 +12,7 @@ import net.minecraftforge.fml.common.Mod;
 @Mod.EventBusSubscriber(modid = InfinityButtons.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ParticleEvent {
     @SubscribeEvent
+    @OnlyIn(Dist.CLIENT)
     public static void registerParticleFactories(final RegisterParticleProvidersEvent event ) {
         event.register(InfinityButtonsParticleTypes.DIAMOND_SPARKLE.get(), DiamondSparkleParticle.DiamondSparkleProvider::new);
     }
